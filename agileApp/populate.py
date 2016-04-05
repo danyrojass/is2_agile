5a9c44c1e2a3796b7c0606ecae136652c268c5e8
Alfredo Barrios <alfbarrios2010@gmail.com>
2016-04-04 22:34:59 -0400

    feat(poblarBD): se agrega archivo que poblará la base de datos
    Con este archivo se crea la población en la base de datos

---
+++ agileApp/populate.py
@@ -0,0 +1,16 @@
+
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agile.settings')
+
+def start_populating():
+    pass
+
+
+
+if __name__ == '__main__':
+    print "Populating the data base..."
+    start_populating()
+    print "We are Done... Thanks for wait :-)"
+    
