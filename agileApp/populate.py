ba0a217ac1559731e682f4c353665af7ac7120d2
danyrjoass <dany@dany-HP>
2016-04-06 19:15:32 -0400

    ref(population): Arreglo de funciones
    Agregamos nuevas funciones para generar usuarios.

--- agileApp/populate.py
+++ agileApp/populate.py
@@ -1,27 +1,37 @@
 # -*- encoding: utf-8 -*-
 import os
 import sys
+from django.contrib.auth.models import User
 
+def crear_usuario(nombre, apellido, username, password, email, activo):
+    user = User()
+    user.first_name = nombre
+    user.last_name = apellido
+    user.username = username
+    user.set_password(password)
+    user.email = email
+    user.is_active = activo
+    user.save()
+    return user
 
 def start_populating(tipoAmbiente, nombreTag):
     
     if(tipoAmbiente==1):
         
         os.system("source ../agileEnv/bin/activate")
+        
+        crear_usuario('Alfredo', 'Barrios', 'abarrios', 'a123', 'alfbarrios2010@gmail.com', True)
+        crear_usuario('Christian', 'Pérez', 'cperez', 'a123', 'criper123@gmail.com', True)
+        crear_usuario('Luis', 'Soto', 'lsoto', 'a123', 'lutyma89@gmail.com', False)
+        crear_usuario('Jorge', 'Rojas', 'jrojas', 'a123', 'danyrojassimon@gmail.com', False)
+        
         os.system("python ../manage.py runserver")
-
-
-
-
-
-
-
-    
-
-
-
-
-
+        os.system("python ../deactivate")
+    else:
+        crear_usuario('Alfredo', 'Barrios', 'abarrios', 'a123', 'alfbarrios2010@gmail.com', True)
+        crear_usuario('Christian', 'Pérez', 'cperez', 'a123', 'criper123@gmail.com', True)
+        crear_usuario('Luis', 'Soto', 'lsoto', 'a123', 'lutyma89@gmail.com', False)
+        crear_usuario('Jorge', 'Rojas', 'jrojas', 'a123', 'danyrojassimon@gmail.com', False)
 
 if __name__ == '__main__':
     print "Populating the data base..."
