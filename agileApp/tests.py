53541984bdf354d77910a652f535796f79dd7339
criper <criper92@hotmail.com>
2016-04-06 15:47:01 -0400

    feat(test): Pruebas Unitarias
    Realizacion de Pruebas Unitarias

--- agileApp/tests.py
+++ agileApp/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, AnonymousUser
+from mx.Tools.Tools import username
+from django.test.client import Client
+from django.contrib.auth import authenticate, login
+from django.test.client import Client, RequestFactory
+from django.test import TestCase, RequestFactory
+from agileApp.views import ingresar
 
-# Create your tests here.
+class test_templates(TestCase):
+    
+      def setUp(self):
+            self.client = Client()
+    
+    
+      def test_ver_adminPage(self):
+        """
+       Prueba de visualizacion del template de Pagina Admin
+       """
+        self.factory = RequestFactory()
+        resp= self.factory.get('/templates/inicio_admin')
+        self.assertTemplateUsed('inicio_admin.html')
+      def test_ver_login(self):
+        """
+       Prueba de visualizacion del template de login
+       """
+        self.factory = RequestFactory()
+        resp= self.factory.get('/templates/login')
+        self.assertTemplateUsed('login.html')
+        
+      def test_ingresar(self):
+        """
+        Prueba de acceso a la pagina de ingreso
+        """
+        resp = self.client.get('/ingresar/')
+        resp.user = AnonymousUser()
+        self.assertEqual(resp.status_code, 200)
