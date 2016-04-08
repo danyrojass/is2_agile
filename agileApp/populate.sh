64a22cb76154718175c444652e0ebc3801a285f1
danyrjoass <dany@dany-HP>
2016-04-08 11:28:03 -0400

    ref(populate): Arreglo de funciones
    Agregamos la función necesaria para hacer checkout del tag.

--- agileApp/populate.sh
+++ agileApp/populate.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-if [ "$1" -eq "1" ]; then
-	source ../agileEnv/bin/activate
-	python ../manage.py populate_db
-	python ../manage.py runserver
-	deactivate
-else
-	python ../manage.py populate_db
+if [ "$#" -eq "2" ]; then
+	re='^[0-9]+$'
+	if ! [[ $1 =~ $re ]] ; then
+  		echo "El primer parámetro no es un número." >&2;
+	else
+		nombreTag=$2
+		
+		git checkout $nombreTag;
 
+		if [ "$1" -eq "1" ]; then
+			source ../agileEnv/bin/activate
+			python ../manage.py populate_db
+			firefox 127.0.0.1:8000
+			python ../manage.py runserver
+			deactivate
+		else
+			python ../manage.py populate_db
+			firefox 0.0.0.0
+		fi
+	fi
+
+else
+	echo "La cantidad de parámetros es diferente a dos (2).";
 fi
