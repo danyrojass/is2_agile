c90bef9eb75907933cdc3358c730782069a303dd
danyrjoass <dany@dany-HP>
2016-04-08 15:40:49 -0400

    ref(pruebas unitarias): Arreglo de tests
    Cambiamos algunos tests y agregamos uno nuevo para el login.

--- agileApp/tests.py
+++ agileApp/tests.py
@@ -1,24 +1,35 @@
-from django.test import TestCase
 from django.contrib.auth.models import User, AnonymousUser
+from django.core import mail
+from random import choice
+from django.contrib.auth.hashers import make_password, check_password
 from django.test.client import Client
-from django.contrib.auth import authenticate, login
-from django.test.client import Client, RequestFactory
 from django.test import TestCase, RequestFactory
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from datetime import timedelta, date, datetime
+
+class test_login(TestCase):
+    
+    def test_login(self):
+        """
+        Prueba del login de un usuario
+        """
+        #Se crea un usuario para la prueba
+        us = User()
+        us.username = "agile_prueba"
+        us.set_password('admin123')
+        us.email = "agile@sgpa.com"
+        us.is_active = True
+        us.save()
+        
+        c = Client()
+        self.assertTrue(c.login(username = 'agile_prueba', password = 'admin123'), "El usuario no se ha podido loguear")
 
 class test_templates(TestCase):
     
     def setUp(self):
         self.client = Client()
-    
-    
-    def test_ver_adminPage(self):
-        """
-        Prueba de visualizacion del template de Pagina Admin
-        """
-        self.factory = RequestFactory()
-        resp= self.factory.get('/templates/inicio_admin')
-        self.assertTemplateUsed('inicio_admin.html')
-    
+
     def test_ver_login(self):
         """
         Prueba de visualizacion del template de login
